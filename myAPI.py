8a339eb8a1ba64adc479db810e7056147500541d
Lucas Gabriel <lucas@nitro-fenix.nitro-fenix>
2023-01-28 22:01:12 -0300

    b0.2

--- myAPI.py
+++ myAPI.py
@@ -18,14 +18,23 @@ app.add_middleware(
     allow_headers=["*"],
 )
 
-knn = KNeighborsClassifier(n_neighbors=3)
-data = Bc()
-knn.fit(data.data, data.target)
+knn = None
+
+@app.get('/train')
+def train():
+    knn = KNeighborsClassifier(n_neighbors=3)
+    data = Bc()
+    knn.fit(data.data, data.target)
 
 
 @app.get('/consult')
 def consult(aluno: str):
+    if knn == None:
+        return '-1'
     al = json.loads(aluno)
     return str(knn.predict([al['notas']])[0])
 
 
+@app.get('/consultAll')
+def consAll(dataset):
+    return "Acho que de Bom"
